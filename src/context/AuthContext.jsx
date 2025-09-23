21a8207558da4098534cf9cd0c6d0eaa0d8fb5b2
alyconr <alyconr@hotmail.com>
2025-09-23 08:58:44 -0500

    subiendo proyecto  buscado con login react

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, use, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+
+const AuthContext = createContext()
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+
+  const login = (username, password) => {
+    // Aquí iría la lógica real de autenticación
+    if (username === 'admin' && password === 'password') {
+      setUser({ username: 'admin' })
+      navigate('/usuarios')
+    } else {
+      alert('Credenciales incorrectas')
+    }
+  }
+
+  const logout = () => {
+    setUser(null)
+    navigate('/login')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
